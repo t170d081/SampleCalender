c874e4d948d8f13714f342c1db8c32ce9ae4a982
t170d081 <t170d081@gunma-u.ac.jp>
2020-06-21 18:19:04 +0900

    next(1) commit --SQLite

---
+++ app/src/main/java/com/example/samplecalender/MyOpenHelper.java
@@ -0,0 +1,27 @@
+package com.example.samplecalender;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class MyOpenHelper extends SQLiteOpenHelper {
+
+    //コンストラクタ
+    public MyOpenHelper(Context context) {
+        super(context, "NameAgeDB", null, 1);
+    }
+
+    //SQLのコマンドしてくれている？
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table Schedule(" + " Date text not null," + "TimeDivision text," +
+                "Plans text" + ");");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+}
---
+++ app/src/main/java/com/example/samplecalender/ShowDataBase.java
@@ -0,0 +1,41 @@
+package com.example.samplecalender;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class ShowDataBase extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_data_base);
+
+        //縦に表示する配列的な何か
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        setContentView(layout);
+
+        //SQLセットアップ
+        MyOpenHelper helper = new MyOpenHelper(this);
+        SQLiteDatabase db = helper.getReadableDatabase();
+
+        // queryメソッドの実行例(おそらくDBのテーブルについて)
+        Cursor c = db.query("Schedule", new String[] { "Date", "TimeDivision","Plans"},
+                null, null, null, null, null);
+
+        //不明ｗ
+        boolean mov = c.moveToFirst();
+        while (mov) {
+            TextView textView = new TextView(this);
+            textView.setText(String.format("%s : %s : %s ", c.getString(0),
+                    c.getString(1),c.getString(2)));
+            mov = c.moveToNext();
+            layout.addView(textView);
+        }
+        c.close();
+        db.close();
+    }
+}
